2af01c44fcc0388bf69d6c0249415c6c724121d9
Komei333 <Hal236236@gmail.com>
2023-03-04 18:17:22 +0900

    タイトルのBGMとSE

---
+++ Assets/Nibe/Script/optionBGM.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using static UnityEditor.Progress;
+using UnityEngine.UI;
+
+public class optionBGM : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+
+    public AudioClip bgm;
+    AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Component‚ğæ“¾
+        audioSource = GetComponent<AudioSource>();
+
+        //BGM‚ğÄ¶‚·‚é
+        audioSource.PlayOneShot(bgm);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //audioMixer‚É‘ã“ü
+        audioMixer.SetFloat("BGM", PlayerPrefs.GetFloat("BGM"));
+    }
+}
