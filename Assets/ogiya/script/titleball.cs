cb0593b4427daf077886b58165db5f28b36cea18
iven1280 <ibuki08210821@yahoo.co.jp>
2023-05-06 00:15:35 +0900

    タイトル背景修正版

---
+++ Assets/ogiya/script/titleball.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleball : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = new Vector3(1f, 0, 0);
+        rb.AddForce(movement * 1f);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
