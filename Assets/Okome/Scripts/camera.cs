140d915548548fb2e3f0d308631a3cf7144fcb0e
M0A21068 <m0a2106863@edu.teu.ac.jp>
2023-06-18 21:11:59 +0900

    カメラとプレイヤーが別に動くようにした。プレイヤーが違すぎるときにプレイヤーに貫通しないようにした。

--- Assets/Okome/Scripts/camera.cs
+++ Assets/Okome/Scripts/camera.cs
@@ -16,6 +16,16 @@ public class camera : MonoBehaviour
     //このカメラのローカル座標を格納
     private Vector3 Position;
 
+    //カメラを正面に戻したときの位置
+    private Vector3 firstPosition;
+
+
+    private Vector3 prePosition;
+    private Vector3 currentPosition;
+
+    //カメラとプレイヤーが最も近い距離
+    public float nearestCameraPosition = 3.0f;
+
     //カメラがものに当たっている情報
     private RaycastHit Hit;
 
@@ -31,28 +41,39 @@ public class camera : MonoBehaviour
     void Start()
     {
         Parent = transform.root.gameObject;
-
-        Position = transform.localPosition;
-
+        firstPosition = transform.localPosition;
+        if (transform.root.gameObject.CompareTag("Player"))
+            gameObject.transform.parent = null;
+        Position = transform.position;       
         Distance = Vector3.Distance(Parent.transform.position, transform.position);
-
-    }
-    void Update()
-    { 
-        //Rotate();
-        AvoidWall();
+        currentPosition = Parent.transform.position;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        //子オブジェクトから外してカメラが移動するようにする
+        prePosition = currentPosition;
+        currentPosition = Parent.transform.position;
+        transform.position = transform.position + (currentPosition - prePosition);
 
+        if(Input.GetKey(KeyCode.H))
+        {
+            Vector3 bhindAngle = currentPosition + (Parent.transform.up * firstPosition.y) + (Parent.transform.forward * firstPosition.z);
+            transform.position = bhindAngle;
+        }
+    }
 
+    void Update()
+    {
+        Rotate();
+        AvoidWall();
     }
 
     void Rotate()
     {
         float cal = PlayerPrefs.GetFloat("Sensi");  //マウス感度を取得してる（重要）
         transform.LookAt(Parent.transform);
+        float rotX = Input.GetAxis("Mouse X") * rotationSpeed.x;
         float rotY = Input.GetAxis("Mouse Y") * rotationSpeed.y;
 
         if (transform.forward.y * -1f > 0.8f && rotY < 0)
@@ -68,6 +89,7 @@ public class camera : MonoBehaviour
         if (Gamepad.current == null)
         {
             //プレイヤーを中心に回転する
+            transform.RotateAround(playerObject.transform.position, transform.up, rotX * cal);
             transform.RotateAround(playerObject.transform.position, transform.right, -rotY * cal);
         }
         else
@@ -87,11 +109,13 @@ public class camera : MonoBehaviour
             if (rotY == 0)
             {
                 //プレイヤーを中心に回転する
+                transform.RotateAround(playerObject.transform.position, transform.up, v.x * 0.5f * cal);
                 transform.RotateAround(playerObject.transform.position, transform.right, -v.y * 0.5f * cal);
             }
             else
             {
                 //プレイヤーを中心に回転する
+                transform.RotateAround(playerObject.transform.position, transform.up, rotX * cal);
                 transform.RotateAround(playerObject.transform.position, transform.right, -rotY * cal);
             }
         }
@@ -102,19 +126,25 @@ public class camera : MonoBehaviour
         //壁を避けていないとき
         if (avoidWall == false)
         {
-            Position = transform.localPosition;
+            Position = transform.position;
         }
         //プレイヤーとカメラの間にオブジェクトがあるとき
-        if (Physics.SphereCast(Parent.transform.position, 1f, (transform.position - Parent.transform.position　).normalized, out Hit, Distance, Mask))
+        if (Physics.SphereCast(Parent.transform.position, 0.5f, (transform.position - Parent.transform.position　).normalized, out Hit, Distance, Mask))
         {      
-            //カメラの位置を移動
-            transform.position = Parent.transform.position + (transform.position - Parent.transform.position).normalized * Hit.distance;
             avoidWall = true;
+            if (Hit.distance < nearestCameraPosition + 0.1f)
+            {
+                //カメラとプレイヤーの距離が近いときカメラが中に埋まらないように
+                transform.position = Parent.transform.position + (transform.position - Parent.transform.position).normalized * nearestCameraPosition;
+            }
+            else
+                //カメラの位置を移動
+                transform.position = Parent.transform.position + (transform.position - Parent.transform.position).normalized * Hit.distance;
         }
         else//プレイヤーとカメラの間にオブジェクトがないとき
         {
             //カメラを元の位置まで移動する
-            transform.localPosition = Vector3.Lerp(transform.localPosition, Position, 2);
+            transform.localPosition = Vector3.Lerp(transform.position, Position, 2);
             if (avoidWall == true)
             {
                 avoidWall = false;
