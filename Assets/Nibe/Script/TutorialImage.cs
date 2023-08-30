9307db5d67353cd9ad382ac6d3edefd89c9a59db
LAPTOP-3R5L2OI6\Kei <3n1.0916@gmail.com>
2023-08-30 11:38:54 +0900

    EC2023前にデバッグし、バグやエラーがあった箇所を修正しました。

--- Assets/Nibe/Script/TutorialImage.cs
+++ Assets/Nibe/Script/TutorialImage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
+
 
 public class TutorialImage : MonoBehaviour
 {
