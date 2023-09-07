a59864fe207ee35eefe461f9ac95dd046cacdfb3
galleria2303_r\tutgp <m0a2127961@edu.teu.ac.jp>
2023-09-07 18:26:30 +0900

    クレジットをPS4コンで動かせるようにしました。

--- Assets/Keke/Script/controllerscroll.cs
+++ Assets/Keke/Script/controllerscroll.cs
@@ -9,16 +9,28 @@ public class controllerscroll : MonoBehaviour
     // Start is called before the first frame update
     // Update is called once per frame
     public Scrollbar ScrollBar;
+    private void Update()
+    {
+        MoveScrollView();
+    }
 
     public void MoveScrollView()
     {
-        if (Input.GetAxisRaw("Vertical") >0)
+
+        if (Input.GetAxisRaw("Vertical") > 0)
         {
-            ScrollBar.value = ScrollBar.value + 0.01f;
+            if (ScrollBar.value <= 1.0)
+            {
+                ScrollBar.value = ScrollBar.value + 0.001f;
+            }
         }
         if (Input.GetAxisRaw("Vertical") < 0)
         {
-            ScrollBar.value = ScrollBar.value - 0.01f;
+            if (ScrollBar.value >= 0.0)
+            {
+                ScrollBar.value = ScrollBar.value - 0.001f;
+            }
         }
+
     }
 }
