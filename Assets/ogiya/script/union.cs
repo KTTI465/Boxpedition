e8772fdec37cf59012eb78448c9055a8ad3fe414
iven1280 <ibuki08210821@yahoo.co.jp>
2024-02-16 08:08:47 +0900

    プログラムの微修正

--- Assets/ogiya/script/union.cs
+++ Assets/ogiya/script/union.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
+using UnityEngine.InputSystem;
 
 public class union : MonoBehaviour
 {
     public GameObject main;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //PS4コントローラーの設定
+    public bool ps40 = false;
 
     // Update is called once per frame
     void Update()
@@ -17,17 +16,34 @@ public class union : MonoBehaviour
         Transform Box = this.transform;
         Vector3 cube = Box.transform.position;
         Vector3 capsule = main.transform.position;
+
+
  
         float arie = Vector3.Distance(cube, capsule);
 
-        if(arie < 3.0f && Input.GetKey(KeyCode.R))
+        if(arie < 3.0f && ps40 == true)
         {
             main.transform.position = new Vector3(cube.x, cube.y + 1, cube.z);
             this.gameObject.transform.parent = main.transform;
         }
-        if(Input.GetKey(KeyCode.T))
+
+    }
+
+    void GetPS40()
+    {
+        //コントローラーのボタンを認識
+        if (Gamepad.current != null)
         {
-            this.gameObject.transform.parent = null;
+            if (Gamepad.current.buttonEast.isPressed)
+            {
+                //〇ボタンを押した時
+                ps40 = true;
+            }
+            else
+            {
+                //そうでなければ判定なし
+                ps40 = false;
+            }
         }
     }
 }
